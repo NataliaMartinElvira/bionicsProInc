c2877ac00e0b56e160dc81f97ea94e5177b1578f
silvi <silvi@LAPTOP-UK5K2O37>
2021-03-12 22:57:29 +0100

    Removing unused interface

--- src/bionicsproInc/db/ifaces/DBManager.java
+++ src/bionicsproInc/db/ifaces/DBManager.java
@@ -1,6 +1,17 @@
 package bionicsproInc.db.ifaces;
 
+import java.util.List;
+
+import bionicsproInc.db.pojos.Engineer;
+
 public interface DBManager {
 	public void connect();
 	public void disconnect();
+	//Add new engineer
+		public void hireEngineer(Engineer eng);
+		
+		//getting an existed engineer
+		public Engineer getEngineer(int id);
+		
+		public List<Engineer> search_by_name(String NAME_SURNAME);
 }

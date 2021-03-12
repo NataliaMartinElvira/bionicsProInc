ed051a9aa11b4fbf9582ccb6af224fefe25c3451
nme20 <nme20@10.60.58.15>
2021-03-12 19:35:03 +0100

    update

--- src/bionicsproInc/db/jdbc/JDBCManager.java
+++ src/bionicsproInc/db/jdbc/JDBCManager.java
@@ -30,16 +30,16 @@ public class JDBCManager {
 						   + "(id INTEGER  PRIMARY KEY AUTOINCREMENT,"
 						   + " name TEXT NOT NULL, "
 						   + " bodypart  TEXT UNIQUE NOT NULL,"
-						   + "price FLOAT NOT NULL,"
-						   + "date_creation DATE NOT NULL,"
-						   + "photo BLOB,"
-						   + "percentagefb FLOAT NOT NULL )";
+						   + " price REAL NOT NULL,"
+						   + " date_creation DATE NOT NULL,"
+						   + " photo BLOB,"
+						   + " percentagefb REAL NOT NULL )";
 				stmt1.executeUpdate(sql1);
 				sql1 = "CREATE TABLE material "
 						   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
 						   + " name     TEXT     NOT NULL UNIQUE, "
-						   + "price FLOAT NOT NULL,"
-						   +"product_id INTEGER REFERENCE products(id),"
+						   + " price REAL NOT NULL,"
+						   + " product_id INTEGER REFERENCE products(id),"
 						   + " amount   INTEGER	 NOT NULL)";
 				stmt1.executeUpdate(sql1);
 				sql1= "CREATE TABLE customer "
@@ -47,13 +47,13 @@ public class JDBCManager {
 						   + " first_name     TEXT     NOT NULL, "
 						   + " last_name   TEXT  	NOT NULL, "
 						   + " age INTEGER NOT NULL,"
-						   + "gender VARCHAR(10) check (gender in ('Male','Female'))"
-						   + "phone INTEGER NOT NULL"
-						   + "email TEXT NOT NULL"
-						   + "street TEXT NOT NULL"
-						   + "city TEXT NOT NULL"
-						   + "postal_code INTEGER NOT NULL"
-						   + "order_id INTEGER)";
+						   + " gender VARCHAR(10) check (gender in ('Male','Female'))"
+						   + " phone INTEGER NOT NULL"
+						   + " email TEXT NOT NULL"
+						   + " street TEXT NOT NULL"
+						   + " city TEXT NOT NULL"
+						   + " postal_code INTEGER NOT NULL"
+						   + " order_id INTEGER)";
 				stmt1.executeUpdate(sql1);
 				stmt1.close();
 		}catch(SQLException e) {

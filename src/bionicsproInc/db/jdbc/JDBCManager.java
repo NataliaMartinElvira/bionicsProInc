680af998c672beeefc00391e7a3864ac96e38c59
Rixxye <r.oriol@usp.ceu.es>
2021-03-12 19:45:32 +0100

    .

--- src/bionicsproInc/db/jdbc/JDBCManager.java
+++ src/bionicsproInc/db/jdbc/JDBCManager.java
@@ -27,33 +27,33 @@ public class JDBCManager {
 		try {
 			stmt1 = c.createStatement();
 			String sql1 = "CREATE TABLE products "
-						   + "(id INTEGER  PRIMARY KEY AUTOINCREMENT,"
+						   + " (id INTEGER  PRIMARY KEY AUTOINCREMENT,"
 						   + " name TEXT NOT NULL, "
 						   + " bodypart  TEXT UNIQUE NOT NULL,"
-						   + "price FLOAT NOT NULL,"
-						   + "date_creation DATE NOT NULL,"
-						   + "photo BLOB,"
-						   + "percentagefb FLOAT NOT NULL )";
+						   + " price FLOAT NOT NULL,"
+						   + " date_creation DATE NOT NULL,"
+						   + " photo BLOB,"
+						   + " percentagefb FLOAT NOT NULL )";
 				stmt1.executeUpdate(sql1);
 				sql1 = "CREATE TABLE material "
-						   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+						   + " (id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
 						   + " name     TEXT     NOT NULL UNIQUE, "
-						   + "price FLOAT NOT NULL,"
-						   +"product_id INTEGER REFERENCE products(id),"
+						   + " price FLOAT NOT NULL,"
+						   +" product_id INTEGER REFERENCE products(id),"
 						   + " amount   INTEGER	 NOT NULL)";
 				stmt1.executeUpdate(sql1);
 				sql1= "CREATE TABLE customer "
-						   + "(person_id INTEGER  PRIMARY KEY AUTOINCREMENT,"
+						   + " (person_id INTEGER  PRIMARY KEY AUTOINCREMENT,"
 						   + " first_name     TEXT     NOT NULL, "
 						   + " last_name   TEXT  	NOT NULL, "
 						   + " age INTEGER NOT NULL,"
-						   + "gender TEXT CHECK (gender = 'Male' OR gender = 'Female')"
-						   + "phone INTEGER NOT NULL"
-						   + "email TEXT NOT NULL"
-						   + "street TEXT NOT NULL"
-						   + "city TEXT NOT NULL"
-						   + "postal_code INTEGER NOT NULL"
-						   + "order_id INTEGER)";
+						   + " gender TEXT CHECK (gender = 'Male' OR gender = 'Female')"
+						   + " phone INTEGER NOT NULL"
+						   + " email TEXT NOT NULL"
+						   + " street TEXT NOT NULL"
+						   + " city TEXT NOT NULL"
+						   + " postal_code INTEGER NOT NULL"
+						   + " order_id INTEGER)";
 				stmt1.executeUpdate(sql1);
 				stmt1.close();
 		}catch(SQLException e) {

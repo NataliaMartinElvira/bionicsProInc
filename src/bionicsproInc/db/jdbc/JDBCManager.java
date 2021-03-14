5d2f36ce8a79e4de8eb18a5dcdec106f60684218
Rixxye <r.oriol@usp.ceu.es>
2021-03-14 12:22:20 +0100

    Created insert for customer

--- src/bionicsproInc/db/jdbc/JDBCManager.java
+++ src/bionicsproInc/db/jdbc/JDBCManager.java
@@ -1,11 +1,14 @@
 package bionicsproInc.db.jdbc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.*;
 import bionicsproInc.db.ifaces.DBManager;
 import bionicsproInc.db.pojos.*;
 
- public class JDBCManager implements DBManager{
+public class JDBCManager implements DBManager {
 	private Connection c;
+
 	public void connect() {
 		try {
 			// Open database connection
@@ -22,68 +25,43 @@ import bionicsproInc.db.pojos.*;
 			e.printStackTrace();
 		}
 	}
-	
-	private void createTables(){
+
+	private void createTables() {
 		Statement stmt1;
 		try {
 			stmt1 = c.createStatement();
-			String sql1 = "CREATE TABLE products "
-						   + "(id INTEGER  PRIMARY KEY AUTOINCREMENT,"
-						   + " name TEXT NOT NULL, "
-						   + " bodypart  TEXT UNIQUE NOT NULL,"
-						   + " price REAL NOT NULL,"
-						   + " date_creation DATE NOT NULL,"
-						   + " photo BLOB )";
-				stmt1.executeUpdate(sql1);
-				sql1 = "CREATE TABLE material "
-						   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-						   + " name     TEXT     NOT NULL UNIQUE, "
-						   + " price REAL NOT NULL,"
-						   + " product_id INTEGER REFERENCE products(id),"
-						   + " amount   INTEGER	 NOT NULL)";
-				stmt1.executeUpdate(sql1);
-				sql1= "CREATE TABLE customer "
-						   + "(person_id INTEGER  PRIMARY KEY AUTOINCREMENT,"
-						   + " first_name     TEXT     NOT NULL, "
-						   + " last_name   TEXT  	NOT NULL, "
-						   + " age INTEGER NOT NULL,"
-						   + " gender TEXT CHECK(gender = 'Male' OR gender = 'Female')"
-						   + " phone INTEGER NOT NULL"
-						   + " email TEXT NOT NULL"
-						   + " street TEXT NOT NULL"
-						   + " city TEXT NOT NULL"
-						   + " postal_code INTEGER NOT NULL"
-						   + " order_id INTEGER)";
-				stmt1.executeUpdate(sql1);
-				sql1 = "CREATE TABLE Engineer "
-						   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-						   + " Name_surname     TEXT     NOT NULL UNIQUE, "
-						   + " contract_starting_date DATE NOT NULL UNIQUE,"
-						   + " contract_ending_date DATE NOT NULL,"
-						   + " current_service TEXT NOT NULL,"
-						   + " salary REAL NOT NULL,"
-						   + " bonus REAL NOT NULL,"
-						   + " project_achieved INTEGER NOT NULL,"
-						   + " experience_in_years INTEGER NOT NULL,"
-						   + " date_of_birth DATE NOT NULL,"
-						   + " id_eng_prod INTEGER REFERENCE products(id)";
-				stmt1.executeUpdate(sql1);
-				sql1 = "CREATE TABLE Chatacteristics "
-						   + " length REAL NOT NULL,"
-						   + " width REAL NOT NULL,"
-						   + " weight REAL NOT NULL,"
-						   + " joint_numb INTEGER NOT NULL,"
-						   + " flexibility_scale INTEGER NOT NULL,"
-						   + " id_char_prod INTEGER REFERENCE products(id)";
-				stmt1.executeUpdate(sql1);		
-				stmt1.close();
-		}catch(SQLException e) {
-			if(!e.getMessage().contains("alredy exists")) {
+			String sql1 = "CREATE TABLE products " + "(id INTEGER  PRIMARY KEY AUTOINCREMENT," + " name TEXT NOT NULL, "
+					+ " bodypart  TEXT UNIQUE NOT NULL," + " price REAL NOT NULL," + " date_creation DATE NOT NULL,"
+					+ " photo BLOB )";
+			stmt1.executeUpdate(sql1);
+			sql1 = "CREATE TABLE material " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " name     TEXT     NOT NULL UNIQUE, " + " price REAL NOT NULL,"
+					+ " product_id INTEGER REFERENCE products(id)," + " amount   INTEGER	 NOT NULL)";
+			stmt1.executeUpdate(sql1);
+			sql1 = "CREATE TABLE customer " + "(person_id INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " first_name     TEXT     NOT NULL, " + " last_name   TEXT  	NOT NULL, "
+					+ " age INTEGER NOT NULL," + " gender TEXT CHECK(gender = 'Male' OR gender = 'Female')"
+					+ " phone INTEGER NOT NULL" + " email TEXT NOT NULL" + " street TEXT NOT NULL"
+					+ " city TEXT NOT NULL" + " postal_code INTEGER NOT NULL" + " order_id INTEGER)";
+			stmt1.executeUpdate(sql1);
+			sql1 = "CREATE TABLE engineer " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " Name_surname     TEXT     NOT NULL UNIQUE, " + " contract_starting_date DATE NOT NULL UNIQUE,"
+					+ " contract_ending_date DATE NOT NULL," + " current_service TEXT NOT NULL,"
+					+ " salary REAL NOT NULL," + " bonus REAL NOT NULL," + " project_achieved INTEGER NOT NULL,"
+					+ " experience_in_years INTEGER NOT NULL," + " date_of_birth DATE NOT NULL,"
+					+ " id_eng_prod INTEGER REFERENCE products(id)";
+			stmt1.executeUpdate(sql1);
+			sql1 = "CREATE TABLE chatacteristics " + " length REAL NOT NULL," + " width REAL NOT NULL,"
+					+ " weight REAL NOT NULL," + " joint_numb INTEGER NOT NULL,"
+					+ " flexibility_scale INTEGER NOT NULL," + " id_char_prod INTEGER REFERENCE products(id)";
+			stmt1.executeUpdate(sql1);
+			stmt1.close();
+		} catch (SQLException e) {
+			if (!e.getMessage().contains("alredy exists")) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
 
 	public void disconnect() {
 		try {
@@ -94,25 +72,65 @@ import bionicsproInc.db.pojos.*;
 			e.printStackTrace();
 		}
 	}
+
 	public void addMaterial(Material m) {
 		try {
-			Statement st=c.createStatement();
-			String sql= "INSERT INTO material (name,price,product,amount) "
-			+" VALUES ('m.getName()','m.getPrice()', 'm.getProducts()','m.getAmount')";
+			Statement st = c.createStatement();
+			String sql = "INSERT INTO material (name,price,product,amount) "
+					+ " VALUES ('m.getName()','m.getPrice()', 'm.getProducts()','m.getAmount')";
 			st.executeUpdate(sql);
 			st.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	}
-	/*
-	public void hireEngineer(Engineer eng) {}
-	
-	//getting an existed engineer
-	public Engineer getEngineer(int id) {}
-	
-	public List<Engineer> search_by_name(String NAME_SURNAME){}*/
-	
-}
 
+	}
+
+	public void addCustomer(Customer cust) {
+		try {
+			Statement st = c.createStatement();
+			System.out.println("Please, input the customer info:");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("First name: ");
+			String first_name = reader.readLine();
+			System.out.print("Last name: ");
+			String last_name = reader.readLine();
+			System.out.print("Age: ");
+			String age = reader.readLine();
+			System.out.print("Gender (MALE/FEMALE): ");
+			String gender = reader.readLine();
+			System.out.print("Phone: ");
+			String phone = reader.readLine();
+			System.out.print("Email: ");
+			String email = reader.readLine();
+			System.out.print("Street: ");
+			String street = reader.readLine();
+			System.out.print("City: ");
+			String city = reader.readLine();
+			System.out.print("Postal code: ");
+			String postal_code = reader.readLine();
+			System.out.print("Order id: ");
+			String order_id = reader.readLine();
+			String sql = "INSERT INTO customer (first_name, last_name, age, gender, phone, email, street, city, postal_code, order_id) "
+					+ " VALUES ('" + first_name + "', '" + last_name + "','" + age + "','" + gender + "','" + phone
+					+ "', " + "'" + email + "','" + street + "','" + city + "','" + postal_code + "','" + order_id
+					+ "')";
+			st.executeUpdate(sql);
+			st.close();
+			System.out.println("Customer info processed.");
+			System.out.println("Records inserted.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/*
+	 * public void hireEngineer(Engineer eng) {}
+	 * 
+	 * //getting an existed engineer public Engineer getEngineer(int id) {}
+	 * 
+	 * public List<Engineer> search_by_name(String NAME_SURNAME){}
+	 */
+
+}

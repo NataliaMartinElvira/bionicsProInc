b248fa2fc8f30a1e2f28727b9f7e24c86efe956d
Rixxye <r.oriol@usp.ceu.es>
2021-03-17 20:10:53 +0100

    Created table order

---
+++ src/bionicsproInc/db/pojos/Order.java
@@ -0,0 +1,63 @@
+package bionicsproInc.db.pojos;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Order implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5234210426301713165L;
+	/**
+	 * 
+	 */
+
+	private int order_id;
+	private ArrayList<Product> products;
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + order_id;
+		result = prime * result + ((products == null) ? 0 : products.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (order_id != other.order_id)
+			return false;
+		if (products == null) {
+			if (other.products != null)
+				return false;
+		} else if (!products.equals(other.products))
+			return false;
+		return true;
+	}
+
+}

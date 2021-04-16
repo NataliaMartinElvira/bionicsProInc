aa505c21765860ca481f843d4ef04fc4713da698
SilviaGarces <s.garces@usp.ceu.es>
2021-04-16 20:15:38 +0200

    Adding funtions on the interface

--- src/bionicsproInc/db/ifaces/DBManager.java
+++ src/bionicsproInc/db/ifaces/DBManager.java
@@ -32,4 +32,6 @@ public interface DBManager {
 	
 	public List<Product> SearchProductByBody(String bodypart);
 	
+	public List<String> viewBodyparts();
+	
 }
--- src/bionicsproInc/db/jdbc/JDBCManager.java
+++ src/bionicsproInc/db/jdbc/JDBCManager.java
@@ -269,5 +269,6 @@ public class JDBCManager implements DBManager {
 		}
 		return bodyPart;
 	}
+	
 
 }

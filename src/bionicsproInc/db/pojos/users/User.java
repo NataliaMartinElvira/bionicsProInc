f2c2d7c72a8e226248675155cb5caf94bff49160
NataliaMartinElvira <n.martin36@usp.ceu.es>
2021-04-30 19:31:55 +0200

    Creation of User and addition of getters...

--- src/bionicsproInc/db/pojos/users/User.java
+++ src/bionicsproInc/db/pojos/users/User.java
@@ -1,10 +1,124 @@
 package bionicsproInc.db.pojos.users;
 import java.io.Serializable;
+import java.util.Arrays;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name="users")
 public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7993095458725676766L;
+	
+	@Id
+	@GeneratedValue(generator="users")
+	@TableGenerator(name="users", table="sqlite_sequence",
+	    pkColumnName="name", valueColumnName="seq", pkColumnValue="users")
 	private Integer id;
 	private String email;
-	private Integer password;
+	@Lob
+	private byte[] password;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="role_id")
+	private Role role;
+	
+	public User(String email, byte[] password, Role role) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public byte[] getPassword() {
+		return password;
+	}
+
+	public void setPassword(byte[] password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public User() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + Arrays.hashCode(password);
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (!Arrays.equals(password, other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", password=" + Arrays.toString(password) + ", role=" + role
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+
 	
 }

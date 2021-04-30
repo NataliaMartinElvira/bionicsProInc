efab94582107009a659d14d4f5b34444968e4f28
NataliaMartinElvira <n.martin36@usp.ceu.es>
2021-04-30 19:10:33 +0200

    User and Role creation

---
+++ src/bionicsproInc/db/pojos/users/Role.java
@@ -0,0 +1,7 @@
+package bionicsproInc.db.pojos.users;
+import java.io.Serializable;
+
+public class Role implements Serializable{
+	private Integer id;
+	private String name;
+}
---
+++ src/bionicsproInc/db/pojos/users/User.java
@@ -0,0 +1,10 @@
+package bionicsproInc.db.pojos.users;
+import java.io.Serializable;
+
+
+public class User implements Serializable {
+	private Integer id;
+	private String email;
+	private Integer password;
+	
+}
